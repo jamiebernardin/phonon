5ae801c4a8e8e49e360d10bf149290614b53367b
jamiebernardin <jamie.bernardin@gmail.com>
2016-12-29 14:04:21 -0500

    adding animal and pet models

---
+++ api/models/Animal_type.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jbernardin on 12/27/16.
+ */
+module.exports = {
+
+    attributes: {
+        name: {
+            type: 'string'
+        }
+    }
+};
---
+++ api/models/Pet.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jbernardin on 12/27/16.
+ */
+module.exports = {
+
+    attributes: {
+        name: {
+            type: 'string'
+        },
+        animal_type: {
+            model: 'animal_type'
+        },
+    }
+};
